71fc787e9ae63aafa29caa1706d4fecfca8e378b
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-08 16:06:52 -0300

    Comentário

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+// Boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Zelda";
+const texto2 = "Link";
+
+console.log(texto1 === texto2);
+
+//O JavaScript é case-sensitive, ou seja, diferencia maiúsculas e minúsculas.
