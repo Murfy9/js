cb403dfd674068eaec993e1a4042a3f1bc9bc68e
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-08 15:28:10 -0300

    Tipo String

---
+++ type-strings.js
@@ -0,0 +1,13 @@
+// Tipo Strings
+
+const texto = "Óla, Mundo!";
+const stringsDeNumeros = "234656327";
+
+const citaçao = ' Em Bioshock "A man choose, a slave obeys "';
+const personagem = " dita por Andrew Ryan ";
+
+// Concatenaçâo (+)
+
+console.log(citaçao + personagem);
+
+// Templete String
