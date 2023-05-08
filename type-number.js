256a28e1a9c34381313e6d74caf2d05477151f9e
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-08 14:50:13 -0300

    Tipo number

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// Tipo Number
+
+const primeiroNumero = 5;
+const segundoNumero = 7;
+
+const adicao = primeiroNumero + segundoNumero;
+
+console.log(adicao);
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = 0.5;
+
+const divisao = primeiroNumero / numeroPontoFlutuante;
+const multiplicacao = segundoNumero * pontoFlutuanteSemZero;
+
+console.log(divisao);
+console.log(multiplicacao);
+
+// NaN => Not A Number
+
+const zelda = "zelda";
+
+console.log(divisao - zelda);
