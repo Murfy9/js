480597efc5654e5bdb2d687b5b7a956f442356c1
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-09 15:29:24 -0300

    Truthy e falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// Boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// Truthy ou Falsy 0 => false e 1 => true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// Null ==> Vazio ou Nada e Undefined ==> Indefinido
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(typeof varNull);
+
+// O Typeof de Null é um Object e não Null
+
+let numero = 3;
+let texto = "Zelda";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+// Typeof é o tipo que está sendo armazenada nessa variável.
