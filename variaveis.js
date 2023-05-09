c992f07c8d7263b29c1725ffd0f3b2c25c2188f6
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-09 15:03:20 -0300

    Var, let e const

---
+++ variaveis.js
@@ -0,0 +1,38 @@
+// Var
+
+var altura1 = 5;
+var comprimento1 = 7;
+
+area1 = altura1 * comprimento1;
+console.log(area1);
+var area1;
+
+// Let
+
+let retangulo = "retângulo";
+let altura2 = 2;
+let comprimento2 = 10;
+let area2;
+
+if (retangulo === "retângulo") {
+  area2 = altura2 * comprimento2;
+} else {
+  area2 = (altura2 * comprimento2) / 2;
+}
+
+console.log(area2);
+
+// Const
+
+const forma = "triângulo";
+const altura3 = 8;
+const comprimento3 = 15;
+let area3;
+
+if (forma === "quadrado") {
+  area3 = altura3 * comprimento3;
+} else {
+  area3 = (altura3 * comprimento3) / 2;
+}
+
+console.log(area3);
