119168b522ebf98dface19c19d27d4861050fa21
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-09 15:47:38 -0300

    Conversões

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// Tipos De Dado
+
+// Conversão Implícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString); //booleanos false
+console.log(numero == numeroString); //Converte em String true
+console.log(numero + numeroString); //Forma uma concatenação "456456"
+
+// Conversão Explícita
+
+console.log(numero + Number(numeroString));
