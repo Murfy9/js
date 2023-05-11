1c3c877b56d12f31f30f7a764afb88d2e3d440ac
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-11 14:07:04 -0300

    Template String

--- type-strings.js
+++ type-strings.js
@@ -3,12 +3,13 @@
 const texto = "Óla, Mundo!";
 const stringsDeNumeros = "234656327";
 
-const citaçao = ' Em Bioshock "A man choose, a slave obeys "';
-const personagem = " dita por Andrew Ryan ";
+const citacao = 'em Bioshock "A man choose, a slave obeys"';
+const personagem = "dita por Andrew Ryan";
+const opiniao = "uns dos jogos mais incríveis já feito";
 
 // Concatenaçâo (+)
 
-console.log(citaçao + personagem);
+console.log(citacao + personagem);
 
 // Unicode
 
@@ -23,3 +24,7 @@ console.log(tique);
 console.log(hiragana);
 
 // Templete String
+
+const frase = `Uma frase de jogo que gosto é ${citacao}, que é ${personagem} faz com que os jogadores pensam nas ações feitas até ali, que me faz considerar ${opiniao}.`;
+
+console.log(frase);
