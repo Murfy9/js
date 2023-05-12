57720539ea3b9041c14495158e6056ad39223cca
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-12 13:21:27 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,19 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) Declara a Função
+
+function imprimeTexto(texto) {
+  console.log(texto);
+}
+
+// 2) Executa a Função (1 ou + vezes)
+
+imprimeTexto(soma());
+
+function soma() {
+  return 2 + 2;
+}
+
+//console.log(soma());
